9fd4706c16d4ba830b364152c3eee6730e9001bf
NusratNawshin <nawshinnusrat@gmail.com>
2022-12-11 16:15:17 -0500

    updated model

--- Code/train.py
+++ Code/train.py
@@ -21,29 +21,32 @@ def check_preprocess():
 def masked_model():
     model = Sequential()
 
-    model.add(Conv2D(128, kernel_size=5,strides=1,padding='same',input_shape=(35, 35, 3)))
+    model.add(Conv2D(128, kernel_size=6, strides=1, padding='same', input_shape=(32, 32, 3)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=3, strides=2, padding='same'))
 
-    model.add(Conv2D(72,kernel_size=3,strides=1,padding='same'))
+    model.add(Conv2D(72, kernel_size=3, strides=1, padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=5, strides=4, padding='same'))
+    model.add(MaxPooling2D(pool_size=6, strides=2, padding='same'))
 
-    model.add(Conv2D(64,kernel_size=3,strides=1,padding='same'))
+    model.add(Conv2D(64, kernel_size=3, strides=1, padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=5, strides=4, padding='same'))
+    model.add(MaxPooling2D(pool_size=6, strides=2, padding='same'))
 
-    model.add(Conv2D(64,kernel_size=3,strides=1,padding='same'))
+    model.add(Conv2D(64, kernel_size=3, strides=1, padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=4, strides=4, padding='same'))
+    model.add(MaxPooling2D(pool_size=4, strides=2, padding='same'))
 
     model.add(Reshape((-1, 64)))
     # LSTM
     model.add(LSTM(32))
+    model.add(Dense(units=32, activation='sigmoid'))
+    model.add(BatchNormalization())
+
     model.add(Dense(units=1, activation='sigmoid'))
     #
     model.summary()
@@ -68,7 +71,7 @@ def create_valDatagen():
 def create_dataset_generator(datagen, batch_size, path,shuffle):
     return datagen.flow_from_directory(
         directory=str(os.getcwd()) + path,
-        target_size=(35, 35),
+        target_size=(32, 32),
         class_mode="binary", batch_size=batch_size, shuffle=shuffle
 
     )
